a9327ea958874efe3e86ed81f32b03d43c3a3752
Bharat Bhushan <bbhushan@uoguelph.ca>
2022-04-17 20:47:22 -0400

    Added report printing functionality

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+def print_report():
+	print(f"Water: {resources['water']}ml\nMilk:  {resources['milk']}ml\nCoffee: {resources['coffee']}g\nMoney: ${profit:.2f}")
+	     
 
 turn_off = False
 
 
 while not turn_off:
-	user_input = input("What would you like? (espresso/latte/cappuccino): ")
+	user_input = input(" What would you like? (espresso/latte/cappuccino): ")
 
 	if user_input == "off":
 		turn_off = True
+	elif user_input == "report":
+		print_report()
+
 
